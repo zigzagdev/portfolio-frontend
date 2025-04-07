7297c4556519f4afae0b5962564853a850c62a13
Application-drop-up <msts.oo0131@gmail.com>
2025-04-07 22:31:29 +0900

    fix: login-succeeded-message-fix

--- src/lib/login.ts
+++ src/lib/login.ts
@@ -6,7 +6,7 @@ export type LoginFormInput = {
 export async function loginUser(data: LoginFormInput) {
     return new Promise((resolve) => {
         setTimeout(() => {
-            console.log('📦 Mock logined user:', data);
+            console.log('📦 Mock login succeeded user:', data);
             resolve({ message: 'Mock login success' });
         }, 1000);
     });
