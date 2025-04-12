4a7af24c533d012f952dd727a084d19f9f3a4a6a
Application-drop-up <msts.oo0131@gmail.com>
2025-04-12 18:48:21 +0900

    feat: add-logout-protect-routing

--- src/routes/DashBoardRoutes.tsx
+++ src/routes/DashBoardRoutes.tsx
@@ -1,13 +1,21 @@
 import { RouteObject } from 'react-router-dom';
 import AppLayout from '../layouts/AppLayout';
 import Dashboard from '../pages/DashBoard';
+import ProtectedRoute from './ProtectedRoute';
 
 export const dashboardRoutes: RouteObject[] = [
     {
         path: '/',
         element: <AppLayout />,
         children: [
-            { path: '', element: <Dashboard /> }
+            {
+                path: '',
+                element: (
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                )
+            }
         ]
     }
 ];
