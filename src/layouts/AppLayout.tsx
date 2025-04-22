4dc0652c4fc4679d892030038ca1d5273b74b635
Application-drop-up <msts.oo0131@gmail.com>
2025-04-22 20:43:42 +0900

    feat: implement-logined-user-header

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,15 +1,25 @@
 import { Outlet } from 'react-router-dom';
-import {GlobalStyleControl} from "./GlobalStyleControl";
+import { GlobalStyleControl } from "./GlobalStyleControl";
+import Header from '../components/common/Header';
+import { useAuth } from '../context/AuthContext';
 import React from "react";
 
 const AppLayout: React.FC = () => {
+    const { user, loading } = useAuth();
+
+    if (loading) return null;
+
     return (
-        <main className="
-        min-h-screen flex items-center justify-center
-        bg-gradient-to-br from-indigo-50 to-purple-100 px-4 bg-stone-900">
-            <GlobalStyleControl />
-            <Outlet />
-        </main>
+        <>
+            {user && <Header />}
+            <main className="
+                min-h-screen flex items-center justify-center
+                bg-gradient-to-br from-indigo-50 to-purple-100 px-4 bg-stone-900"
+            >
+                <GlobalStyleControl />
+                <Outlet />
+            </main>
+        </>
     );
 };
 
