8dbe39ae71b0a506dafff3c76fb5cb3577702a89
Application-drop-up <msts.oo0131@gmail.com>
2025-05-08 18:32:53 +0900

    fix: routing-fix

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -42,6 +42,7 @@ export const authRoutes: RouteObject[] = [
             isLoggingOut={false}
             errorMsg={''}
             onLogout={() => {}}
+            posts={[]}
         />,
     }
 ];
